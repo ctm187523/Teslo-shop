cc674e66fb5450c84c1ff54e383dcd5de687ec39
Clemente <clemente1875@gmail.com>
2022-12-12 23:19:46 +0100

    instalamos Materiaa UI y archivos y nuevos directorios del zip del videl video 176

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { lightTheme } from '../themes'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+
+    <ThemeProvider theme={ lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+    </ThemeProvider>
+  )
+  
+  
+  
+
 }
